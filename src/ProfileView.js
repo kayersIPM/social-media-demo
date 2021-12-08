139f6d8b59cb799edca3e07228f93132450c22a6
Kody Ayers <kayers@ipmdoctors.com>
2021-12-08 03:15:37 -0800

    trying only email

--- src/ProfileView.js
+++ src/ProfileView.js
@@ -11,30 +11,31 @@ function ProfileView({ currentUser }) {
     // if (currentUser) {
     //     showDeleteButton = currentUser.signInUserSession.accessToken.payload['cognito:groups'].includes('Admins')
     // }
-    let userAttr = { 'custom:ptnum' : currentUser.attributes['custom:ptnum'], 'custom:fullname': currentUser.attributes['custom:fullname'], 'custom:bday': currentUser.attributes['custom:bday']}
-    let hasPtnum = (currentUser.attributes['custom:ptnum'] === '' ?  currentUser.attributes['custom:ptnum']: 'PT12345678')
+    let userAttr = { 'email': currentUser.attributes.email, 'custom:ptnum' : currentUser.attributes['custom:ptnum'], 'custom:fullname': currentUser.attributes['custom:fullname'], 'custom:bday': currentUser.attributes['custom:bday']}
+    // let hasPtnum = (currentUser.attributes['custom:ptnum'] === '' ?  currentUser.attributes['custom:ptnum']: 'PT12345678')
     const updateUser = async( body) => {
         const user = await Auth.currentAuthenticatedUser();
         
         const res = await Auth.updateUserAttributes(user, {
           body
         });
+        return res
       }
-    const handleSave = ({ name, value, previousValue }) => {
-        let cName = ("custom:" + String(name))
-        let body = { cName: value } 
-        userAttr[cName] = value
-        const saveRes = updateUser(body)
+    const handleSave = async ({ name, value, previousValue }) => {
+        
+        let body = { 'email': value } 
+        userAttr['email'] = value
+        const saveRes = await updateUser(body)
         alert(name + ' saved as: ' + value + ' (prev: ' + previousValue + ')' + saveRes);
       };
     return <React.Fragment>
         <div style={{whiteSpace: 'nowrap'}}>
             <strong><label className="mr-2">Full Name: </label></strong>
-            <EditText name="fullname" type="fullname" style={{width: '200px'}} onSave={handleSave} defaultValue={userAttr['custom:fullname'] || 'Kody Test'} inline/>
+            <EditText name="fullname" type="fullname" style={{width: '200px'}} onSave={handleSave} defaultValue={userAttr['custom:fullname']} inline readonly/>
           </div>
           <div style={{whiteSpace: 'nowrap'}}>
             <strong><label className="mr-2">PtNum: </label></strong>
-            <EditText id="ptnum" name="ptnum" style={{width: '200px'}} onSave={handleSave} defaultValue={hasPtnum} inline />
+            <EditText id="ptnum" name="ptnum" style={{width: '200px'}} onSave={handleSave} defaultValue={currentUser.attributes['custom:ptnum']} inline readonly/>
           </div>
           <div style={{whiteSpace: 'nowrap'}}>
             <strong><label className="mr-2">Email Address: </label></strong>
@@ -42,7 +43,7 @@ function ProfileView({ currentUser }) {
           </div>
           <div style={{whiteSpace: 'nowrap'}}>
             <strong><label className="mr-2">Birthdate: </label></strong>
-            <EditText name="bday" type="bday" style={{width: '200px'}} onSave={handleSave} defaultValue={currentUser.attributes['custom:bday'] || '05-16-1995'} inline/>
+            <EditText name="bday" type="bday" style={{width: '200px'}} onSave={handleSave} defaultValue={currentUser.attributes['custom:bday'] || '05-16-1995'} inline readonly/>
           </div>
     </React.Fragment>
     // return <div className="profile">
